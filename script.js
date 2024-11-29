a9c11030eda75d313c3f3b707f11f0f09703c97c
honey4nz <honey.malhotra2222@gmail.com>
2024-11-30 11:06:28 +1300

    added loader and scroll to top

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 //this function will run as soon as the page load
 
-window.onload = function () {
+const updateDots = () => {
   const screenWidth = window.innerWidth;
-
+  const dotCotainer = document.querySelector(".dot-container");
+  dotCotainer.innerHTML = "";
   if (screenWidth >= 1150) {
     for (let i = 0; i < 2; i++) {
       const span = document.createElement("span");
       span.classList.add("dot");
       const dotCotainer = document.querySelector(".dot-container");
-      console.log(dotCotainer);
       dotCotainer.appendChild(span);
     }
   } else if (screenWidth >= 750 && screenWidth < 1150) {
@@ -63,3 +63,69 @@ window.onload = function () {
     });
   });
 };
+
+//scroll up fucntion
+const scrollToTop = () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+};
+//scroll up show
+window.addEventListener("scroll", () => {
+  const scrollBtn = document.querySelector(".scroll-up");
+  if (window.scrollY > 300) {
+    scrollBtn.style.display = "flex";
+  } else {
+    scrollBtn.style.display = "none";
+  }
+});
+
+const menu = document.querySelector(".menu-mobile-list");
+const desktopMenu = document.querySelector(".menu-desktop-list");
+const mobileMenuIcon = document.querySelector(".mobile-menu");
+const openMenu = () => {
+  menu.style.right = "0px";
+};
+
+const closeMenu = () => {
+  menu.style.right = "-600px";
+};
+
+const mediaQuery = window.matchMedia("(max-width:915px)");
+mediaQuery.addEventListener("change", () => {
+  if (mediaQuery.matches) {
+    // menu.style.display = "flex";
+    desktopMenu.style.display = "none";
+    menu.style.right = "-600px";
+    mobileMenuIcon.style.display = "flex";
+  } else {
+    menu.style.right = "-600px";
+    desktopMenu.style.display = "flex";
+    mobileMenuIcon.style.display = "none";
+  }
+});
+
+//loader
+const loader = () => {
+  const loaderContainer = document.createElement("div");
+  const loaderElement = document.createElement("div");
+  loaderContainer.classList.add("loaderContainer");
+  loaderElement.classList.add("loaderElement");
+  const element = document.querySelector(".container");
+  element.appendChild(loaderContainer);
+  loaderContainer.appendChild(loaderElement);
+};
+
+window.onload = () => {
+  //anonymus function for
+  (function () {
+    loader(); //show loader
+    const loaderContainer = document.querySelector(".loaderContainer");
+    setTimeout(() => (loaderContainer.style.display = "none"), 500);
+  })();
+
+  updateDots();
+};
+
+window.addEventListener("resize", updateDots);
